a09b800d89b0c679aa1c418e3e9f51029da0b871
Will Hack <18036720+willhack@users.noreply.github.com>
2023-10-03 17:41:20 -0400

    feat: finish inference basics

--- src/01-inference-basics/03-awaited.problem.ts
+++ src/01-inference-basics/03-awaited.problem.ts
@@ -8,8 +8,6 @@ const getUser = () => {
   });
 };
 
-type ReturnValue = ReturnType<typeof getUser>;
+type ReturnValue = Awaited<ReturnType<typeof getUser>>;
 
-type tests = [
-  Expect<Equal<ReturnValue, { id: string; name: string; email: string }>>,
-];
+type tests = [Expect<Equal<ReturnValue, { id: string; name: string; email: string }>>];
